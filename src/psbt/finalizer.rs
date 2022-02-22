afa9d221c5ea59f1f3a9717e5cf3e18d817c25e1
Jeremy Rubin <j@rubin.io>
2022-02-21 19:11:42 -0800

    Cargo FMT

--- src/psbt/finalizer.rs
+++ src/psbt/finalizer.rs
@@ -315,9 +315,15 @@ fn interpreter_inp_check<C: secp256k1::Verification>(
     {
         let cltv = psbt.unsigned_tx.lock_time;
         let csv = psbt.unsigned_tx.input[index].sequence;
-        let interpreter =
-            interpreter::Interpreter::from_txdata(spk, &script_sig, &witness, cltv, csv, super::get_ctv_hash(&psbt.unsigned_tx, index as u32))
-                .map_err(|e| Error::InputError(InputError::Interpreter(e), index))?;
+        let interpreter = interpreter::Interpreter::from_txdata(
+            spk,
+            &script_sig,
+            &witness,
+            cltv,
+            csv,
+            super::get_ctv_hash(&psbt.unsigned_tx, index as u32),
+        )
+        .map_err(|e| Error::InputError(InputError::Interpreter(e), index))?;
         let iter = interpreter.iter(secp, &psbt.unsigned_tx, index, &utxos);
         if let Some(error) = iter.filter_map(Result::err).next() {
             return Err(Error::InputError(InputError::Interpreter(error), index));
